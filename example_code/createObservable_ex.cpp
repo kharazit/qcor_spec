cfc07abaa13adc858635c7008a5e893ee5cda971
kharazit <58788235+kharazit@users.noreply.github.com>
2020-08-06 13:35:53 -0400

    Update createObservable_ex.cpp

--- example_code/createObservable_ex.cpp
+++ example_code/createObservable_ex.cpp
@@ -4,22 +4,26 @@
 //   Observable* qcor::createObservable (Observable &obsvOp, FieldOperator &fieldOp)
 //
 // Create the Dueteron Hamiltonian (Observable)
-auto H = qcor::createObservable(
+
+//createPauli
+auto H = qcor::createObservable("pauli-operator", 
       "5.907 - 2.1433 X0X1 - 2.1433 Y0Y1 + .21829 Z0 - 6.125 Z1");
 
 auto H = 5.907 - 2.1433 * qcor::X(0) * qcor::X(1) -
            2.1433 * qcor::Y(0) * qcor::Y(1) + .21829 * qcor::Z(0) -
            6.125 * qcor::Z(1);
 
-using namespace qcor;
-auto H = -0.5 * adag(0) * a(0) + 4.0 * adag(0) * a(0) * adag(1) * a(1) - 
-            1.0 * adag(0) * a(1) - 1.0 * adag(0) * a(2) -1.0 * adag(1) * a(0) - 
-            0.5 * adag(1) * a(1) + 4.0 * adag(1) * a(1) * adag(2) * a(2) - 
-            1.0 * adag(1) * a(2) - 1.0 * adag(1) * a(2) - 1.0 *adag(2) * a(0) - 
-            1.0 *adag(2) * a(1) - 0.5 * adag(2) * a(2) + 4.0 * adag(2) * a(2) * adag(0) * a(0);
 
-auto H = createObservable("fermion", "(-0.5, 0.0) * 0^ 0 +"
-                                    "(4.0, 0.0) * 0^ 0 1^ 1 +"
+using namespace qcor;
+//In C
+auto H = createObservable("field-operator", add(multiply( createFieldOperator(-0.5, "create", 0, "fermion"), createFieldOperator(1.0, "annihilate", 0, "fermion")), 
+                                                multiply(createFieldOperator(-4.0, "create", 0, "fermion"), createFieldOperator(1.0, "annihilate", 1, "fermion"),
+                                                createFieldOperator(1.0, "annihilate", 0, "fermion"), createFieldOperator(1.0, "create", 1, "fermion")),
+                                                multiply(
+                                               
+                                               )
+//finish here
+                                    
                                     "(-1.0, 0.0) * 0^ 1 +"
                                     "(-1.0, 0.0) * 0^ 2 +"
                                     "(-1.0, 0.0) * 1^ 0 +"
@@ -30,3 +34,14 @@ auto H = createObservable("fermion", "(-0.5, 0.0) * 0^ 0 +"
                                     "(-1.0, 0.0) * 2^ 1 +"
                                     "(-0.5, 0.0) * 2^ 2 +"
                                     "(4.0, 0.0) * 2^ 2 0^ 0" );
+
+//Equivalently in C++
+auto H = -0.5 * adag(0) * a(0) + 4.0 * adag(0) * a(0) * adag(1) * a(1) - 
+            1.0 * adag(0) * a(1) - 1.0 * adag(0) * a(2) -1.0 * adag(1) * a(0) - 
+            0.5 * adag(1) * a(1) + 4.0 * adag(1) * a(1) * adag(2) * a(2) - 
+            1.0 * adag(1) * a(2) - 1.0 * adag(1) * a(2) - 1.0 * adag(2) * a(0) - 
+            1.0 * adag(2) * a(1) - 0.5 * adag(2) * a(2) + 4.0 * adag(2) * a(2) * adag(0) * a(0);
+
+auto H1 = createObservable(H);
+
+//use H in an example
